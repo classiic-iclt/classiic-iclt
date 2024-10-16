9aaa64cc657fce5c076773ae4c63bad47e6d948a
classiic-iclt <classiiciclotim@gmail.com>
2024-10-16 16:25:08 +0800

    lll

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vite-react-deploy/", // YOUR REPO NAME HERE
+  base: "/classiic-iclt/", // YOUR REPO NAME HERE
 });
